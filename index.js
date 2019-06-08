35ab57a7cb34858758d7ed9f95d8ad788277baac
sirmimi <51514568+sirmimi@users.noreply.github.com>
2019-06-08 18:57:28 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+console.log('The bot is working now ')
+
+
+
+
+
+client.login('NTg2OTUzMTgxOTU5ODE1MTg3.XPvorg.Y5KQDpkDDS7zdwW0rFFfSmy5ROE')
